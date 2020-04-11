f004da6b363c233bced492d7e61ff1c7adb17282
karina heras <karinaheras@MacBook-Pro-de-karina.local>
2020-04-11 16:27:02 +0200

    Aprendiendo hacer test y entendiendo conceptos de TDD

--- src/kataDni.js
+++ src/kataDni.js
@@ -1,21 +1,20 @@
 function letraDni(dni){
 
-if (dni < 0 || dni > 99999999) {
-    return "";
-} else {
-    
-const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+    if (dni.length != 8){
+    return 'el numero del dni debe  contener 8 digitos, rellene con 0 a la izquierda'
+    }
+
+const letras = ['T', 'R', 'W', 'A', 'G', 'M',
+'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 
+'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 const resto = dni%23;
 const letra = letras[resto];
 
 return letra;
 }
-
-}
-
-//const letra = letraDni('03199518');
-//console.log(letra);
-
-
-
 module.exports = letraDni;
+
+
+
+
+
--- src/tests/kataDni.test.js
+++ src/tests/kataDni.test.js
@@ -3,10 +3,10 @@
 
 const kataDni = require('../kataDni');
 
-test('letras and dni', () => {
+test('tiene menos de 8 digitos', () => {
     // ARRANGE
-    const dni = 8
-    const expectedResult = 'letras'
+    const dni = '92'
+    const expectedResult = 'el numero del dni debe  contener 8 digitos, rellene con 0 a la izquierda'
     // ACT
     const result = kataDni(dni)
 
@@ -15,4 +15,147 @@ test('letras and dni', () => {
   });
 
 
- 
+  test('tiene 8 digitos y devuelve la letra que corresponde', () => {
+    // ARRANGE
+    const dni = '00000023'
+    const expectedResult = 'T'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+
+  test('tiene 8 digitos y devuelve la letra R', () => {
+    // ARRANGE
+    const dni = '00000001'
+    const expectedResult = 'R'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra W', () => {
+    // ARRANGE
+    const dni = '00000002'
+    const expectedResult = 'W'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra A', () => {
+    // ARRANGE
+    const dni = '00000003'
+    const expectedResult = 'A'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra G', () => {
+    // ARRANGE
+    const dni = '00000004'
+    const expectedResult = 'G'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra M', () => {
+    // ARRANGE
+    const dni = '00000005'
+    const expectedResult = 'M'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+
+  test('tiene 8 digitos y devuelve la letra Y', () => {
+    // ARRANGE
+    const dni = '00000006'
+    const expectedResult = 'Y'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+  test('tiene 8 digitos y devuelve la letra F', () => {
+    // ARRANGE
+    const dni = '00000007'
+    const expectedResult = 'F'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra P', () => {
+    // ARRANGE
+    const dni = '00000008'
+    const expectedResult = 'P'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra D', () => {
+    // ARRANGE
+    const dni = '00000009'
+    const expectedResult = 'D'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra X', () => {
+    // ARRANGE
+    const dni = '00000010'
+    const expectedResult = 'X'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+
+  test('tiene 8 digitos y devuelve la letra B', () => {
+    // ARRANGE
+    const dni = '00000011'
+    const expectedResult = 'B'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
+
+  test('tiene 8 digitos y devuelve la letra N', () => {
+    // ARRANGE
+    const dni = '00000012'
+    const expectedResult = 'N'
+    // ACT
+    const result = kataDni(dni)
+
+    // ASSERT
+    expect(result).toBe(expectedResult);
+  });
